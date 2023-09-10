ba2c1a4e4bb068fac51870716e76067a5e8bb8ef
boison <boison88@mail.ru>
2023-09-10 15:24:45 +0300

    lesson "templates-extends"

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+#
+# def index(request):
+#     return HttpResponse('article')
+
 
 def index(request):
-    return HttpResponse('article')
-
+    return render(request, 'articles/index.html')
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -6,5 +6,6 @@ def index(request):
         'who': 'World',
     })
 
+
 def about(request):
     return render(request, 'about.html')
