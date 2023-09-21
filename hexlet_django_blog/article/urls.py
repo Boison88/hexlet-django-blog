b109b132501bc285cc97d9ccc380739cbd9d7241
boison <boison88@mail.ru>
2023-09-21 20:26:35 +0300

    lesson "update"

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleCommentsView, ArticleFormCreateView
+from hexlet_django_blog.article.views import (
+    IndexView,
+    ArticleView,
+    ArticleCommentsView,
+    ArticleFormCreateView,
+    ArticleFormView,
+    ArticleFormEditView,
+)
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='article_update'),
     path('<int:id>/', ArticleView.as_view(), name='articles'),
     path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
