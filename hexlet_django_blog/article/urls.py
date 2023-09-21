9308ec17b89d591fc2884ce93b2e34b154ca3609
boison <boison88@mail.ru>
2023-09-21 20:39:26 +0300

    lesson "delete"

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -6,12 +6,14 @@ from hexlet_django_blog.article.views import (
     ArticleFormCreateView,
     ArticleFormView,
     ArticleFormEditView,
+    ArticleFormDeleteView,
 )
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('<int:id>/edit/', ArticleFormEditView.as_view(), name='article_update'),
+    path('<int:id>/delete/', ArticleFormDeleteView.as_view(), name='articles_delete'),
     path('<int:id>/', ArticleView.as_view(), name='articles'),
     path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
